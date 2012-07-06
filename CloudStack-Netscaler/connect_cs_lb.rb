5b23730a7a98f11b3bbffeed68950562ba824a3c
Stephen Bylo <steve@rightscale.com>
2012-07-06 16:21:19 +0800

    ..

--- CloudStack-Netscaler/connect_cs_lb.rb
+++ CloudStack-Netscaler/connect_cs_lb.rb
@@ -7,26 +7,14 @@ require 'openssl'
 require 'cgi'
 require 'json' # gen install json_pure. Note: 'pure' is needed due to a bug on CentOS 5. See http://theforeman.org/issues/1330
 
-# API_URL /  API_KEY  / SECRET_KEY = all defined in INPUTS
+# Note: API_URL / API_KEY / SECRET_KEY / LB_RULE_ID must all be defined in RightScale INPUTS (in the environment)
+
+## TEST values
 API_URL = "https://sdscloudstack.cloudlord.com:12345/client/api"
 API_KEY = "77fZbJ_L0Dd0FSED63sGearUXiJMbGD-VnlXjLZ2snLuzMM_rMMPBFlzAzY_qyNRHfzO7mwvvIWSwssaKozf9A"
 SECRET_KEY = "eADXllxC7tKbGy68mv2hDixtjAaV3uohiK-vTIVSpdZxH4znP79i1N2qZhoZuyPqBGADv87M_fVMeIsS8d2FYA"
 LB_RULE_ID = 111
-
-# This will grab the IP addr of the router with the meta-data for this server (tested on CentOS)
-ip = `cat /var/lib/dhclient/dhclient-eth0.leases | \
-  egrep "dhcp-server-identifier.*[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+" | tail -1 | awk '{print $NF}' | tr -cd "[^0-9.]"`
-
-abort "Cannot find IP addr of the meta-data router/server" if ip == ""
-puts "Will retrieve mete-data from IP = [#{ip}]"
-
-url = 'http://' + ip + '/latest/vm-id'
-vmid = RestClient.send(:get, url)
-
-abort "Cannot retrieve this server's VM ID from #{url}" if vmid == ""
-puts "This VM ID = [#{vmid}]"
-
-puts "Load balancer rule ID = [#{LB_RULE_ID}]"
+## TEST values
 
 def generate_signature(params)
   params.each { |k,v| params[k] = CGI.escape(v.to_s).gsub('+', '%20').downcase }
@@ -72,13 +60,33 @@ def request(params, api_url, method = :get)
   response    
 end
 
+##############################################
+# Fetch the VM ID for this server
+
+# This will grab the IP addr of the router with the meta-data for this server (tested on CentOS)
+ip = `cat /var/lib/dhclient/dhclient-eth0.leases | \
+  egrep "dhcp-server-identifier.*[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+" | tail -1 | awk '{print $NF}' | tr -cd "[^0-9.]"`
+
+abort "Cannot find IP addr of the meta-data router/server" if ip == ""
+puts "Will retrieve mete-data from IP = [#{ip}]"
+
+url = 'http://' + ip + '/latest/vm-id'
+vmid = RestClient.send(:get, url)
+
+abort "Cannot retrieve this server's VM ID from #{url}" if vmid == ""
+puts "This VM ID = [#{vmid}]"
+
+puts "Load balancer rule ID = [#{LB_RULE_ID}]"
+
+##############################################
+# Make CS API request to assign this server to a load balancer
+
 params = {
   'command' => 'assignToLoadBalancerRule',
   'id' => LB_RULE_ID,
   'virtualmachineids' => vmid.to_s 
 }
 
-# Make request to assign this server to load balancer
 response_s = request(params, API_URL, :get)
 tmp = JSON.parse(response_s)
 
@@ -91,6 +99,7 @@ else
   exit 0
 end
 
+##############################################
 # Query and wait for async command to finish ...
 
 params = {
@@ -100,7 +109,7 @@ params = {
 
 puts "Waiting for job (jobid=#{jobid}) to complete"
 
-a_while = 1
+a_while = 2
 tries = 3
 
 begin
@@ -114,7 +123,7 @@ begin
   jr = tmp2['queryasyncjobresultresponse']
 
   tries -= 1
-  a_while += 3
+  a_while += 5
 end while jr['jobstatus'] == 0 and tries > 0
 
 puts "Giving up waiting for job to complete" unless tries > 0
@@ -123,14 +132,15 @@ jobstatus = jr['jobstatus']
 
 case jobstatus
 when 2
-  puts "Request failed (jobstatus=#{jobstatus})"
+  print "Request failed "
 when 1
-  puts "Request succeded (jobstatus=#{jobstatus})"
+  print "Request succeded "
 when 0
-  puts "Request still in progress (jobstatus=#{jobstatus})"
+  print "Request still in progress "
 else
-  puts "Unknown job status #{jobstatus}"
+  print "Unknown result "
 end
+  puts "job status #{jobstatus}"
 
 exit 0
 
